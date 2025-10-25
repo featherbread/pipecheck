cfa609019aaa8fbd0020be88755df6e3342a6172
Alex Hamlin <alex@alexhamlin.co>
2025-10-25 16:43:19 -0700

    Update all the doc comments
    I think this version does a better job of introducing SIGPIPE and
    explaining why silent termination is useful.

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,66 @@
 //! Cross-platform Unix-style handling of broken pipe errors.
+//!
+//! When any call to its underlying writer returns a [`BrokenPipe`](io::ErrorKind::BrokenPipe)
+//! error, a [`Writer`] terminates the current process with a SIGPIPE signal, or exits with code 1
+//! on non-Unix systems.
+//!
+//! # Why is this useful?
+//!
+//! When a process runs in a Unix shell pipeline, it's good form for the process to exit quickly
+//! and silently as soon as its downstream process stops accepting input. Unix simplifies the
+//! handling of this case with the SIGPIPE signal: when a process writes to a pipe where all file
+//! descriptors referring to the read end have been closed, the system sends it this signal, which
+//! by default terminates it.
+//!
+//! The existence of SIGPIPE introduces two challenges. First, it's Unix-specific, so portable CLIs
+//! might not be able to rely on it. Second, a networked server can generate SIGPIPE when writing
+//! to a socket whose client has closed its read end, and terminating the server would break other
+//! clients' connections. Given these challenges, the Rust developers chose to override Unix's
+//! default behavior by globally ignoring SIGPIPE prior to calling `main`, causing all writes to
+//! broken pipes to return a plain [`BrokenPipe`](io::ErrorKind::BrokenPipe) error.
+//!
+//! Unfortunately, a well-meaning CLI that wants to handle broken pipes with a silent exit might
+//! find it difficult using error values alone. Experience shows that real-world Rust libraries
+//! don't always expose enough detail to easily distinguish this from other errors. For example,
+//! the [`source`](std::error::Error::source) implementation in a library's custom error type might
+//! not expose an underlying [`io::Error`] even when traversing the entire chain of sources.
+//!
+//! [`Writer`] instead plumbs this logic directly into every write operation, catching broken pipe
+//! errors and terminating the process before anything else in the call stack has a chance to
+//! obscure them. Unlike an up-front modification of the process-wide SIGPIPE behavior, this
+//! approach is more cross-platform and better scoped to the specific writes where termination is
+//! desired (generally on standard output and error streams).
+//!
+//! Note that termination on Unix invokes the real default behavior of SIGPIPE; `Writer` does not
+//! employ incorrect hacks like exiting with code 141 (mimicking the shell return code of a process
+//! terminated by SIGPIPE).
+//!
+//! # Further Reading
+//!
+//! For further background on SIGPIPE, Rust's handling of it, and cross-platform portability
+//! concerns surrounding broken pipes, see:
+//!
+//! - <https://github.com/rust-lang/rust/issues/62569>
+//! - <https://stackoverflow.com/a/65760807>
+//! - <https://github.com/BurntSushi/ripgrep/issues/200#issuecomment-616884727>
+//!
+//! The concept of `pipecheck` was directly inspired by Go's default behavior for broken pipes:
+//! terminating the program if the write was to a standard output or error stream, and otherwise
+//! returning a plain error. For background on Go's behavior and runtime implementation, see:
+//!
+//! - <https://pkg.go.dev/os/signal#hdr-SIGPIPE>
+//! - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/os/file_unix.go;l=252>
+//! - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/runtime/signal_unix.go;l=333>
 
 use std::io::{self, Write};
 
 /// A writer that silently terminates the program on broken pipe errors.
 ///
 /// When any call to its underlying writer returns a [`BrokenPipe`](io::ErrorKind::BrokenPipe)
-/// error, a `Writer` terminates the current program with a SIGPIPE signal,
-/// or exits with code 1 on non-Unix systems.
+/// error, a [`Writer`] terminates the current process with a SIGPIPE signal, or exits with code 1
+/// on non-Unix systems.
 ///
-/// # Why is this useful?
-///
-/// A Unix program that writes to a broken pipe receives a SIGPIPE signal that,
-/// by default, terminates it immediately. This is usually the optimal behavior
-/// for simple CLI tools used in shell pipelines, but is problematic for
-/// applications like networked servers that may write to a broken pipe any time
-/// a client unexpectedly closes or drops a connection.
-///
-/// Rust overrides the Unix default during program initialization (before `main`
-/// is called), ignoring SIGPIPE so that writes to broken pipes consistently
-/// return a normal [`Result::Err`]. This is helpful for the server use case
-/// described above and provides valuable consistency with non-Unix platforms,
-/// even though it severely hampers some specialized use cases on Unix.
-///
-/// Unfortunately, experience shows that real-world Rust libraries don't
-/// consistently propagate broken pipe errors in a generically detectable way,
-/// such as by including them in error source chains. This makes it difficult
-/// for well-meaning CLIs to replicate the behavior of Unix's default SIGPIPE
-/// handler in a cross-platform manner. `Writer` solves this by plumbing the
-/// required termination logic directly into every write operation, before it
-/// can get lost on its way up the call stack.
-///
-/// On Unix systems in particular, `Writer` accomplishes this by modifying the
-/// process-wide behavior of SIGPIPE immediately before raising that signal
-/// against the current thread, rather than requiring its modification for all
-/// writes in the program. It is also important to note that Unix distinguishes
-/// between process termination by a signal and process termination with an exit
-/// code; `Writer` does not employ incorrect hacks like exiting with code 141
-/// (attempting to replicate the return code of a command terminated by SIGPIPE
-/// in a shell).
-///
-/// For further background on SIGPIPE, Rust's handling of it, and cross-platform
-/// portability concerns surrounding broken pipes, see:
-///
-/// - <https://github.com/rust-lang/rust/issues/62569>
-/// - <https://stackoverflow.com/a/65760807>
-/// - <https://github.com/BurntSushi/ripgrep/issues/200#issuecomment-616884727>
-///
-/// For the implementation in the Go programming language that directly inspired
-/// this approach, see:
-///
-/// - <https://pkg.go.dev/os/signal#hdr-SIGPIPE>
-/// - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/os/file_unix.go;l=252>
-/// - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/runtime/signal_unix.go;l=333>
+/// See [the crate documentation](crate) for more details.
 pub struct Writer<W>(W)
 where
     W: Write;
